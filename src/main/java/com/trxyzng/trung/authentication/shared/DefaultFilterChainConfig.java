33927b1a26b9bb6400fc8bc6083125bd0df52e18
trxyzng <trxyzng37@gmail.com>
2024-02-15 21:51:01 +0700

    remove DefaultFilterChainConfig.java because it cause problem with signin

--- src/main/java/com/trxyzng/trung/authentication/shared/DefaultFilterChainConfig.java
+++ src/main/java/com/trxyzng/trung/authentication/shared/DefaultFilterChainConfig.java
@@ -1,35 +1,34 @@
-package com.trxyzng.trung.authentication.shared;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-@Order(10)
-public class DefaultFilterChainConfig {
-    @Bean
-    public SecurityFilterChain DefaultFilterChain(HttpSecurity http) throws Exception {
-        http
-//            .securityMatcher("/signin/check-google-jwt-token")
-//            .addFilterBefore(new RefreshTokenValidationFilter(), RequestCacheAwareFilter.class)
-            .cors(Customizer.withDefaults())
-            .csrf(AbstractHttpConfigurer::disable)
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .anonymous(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .logout(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> {
-                auth.anyRequest().permitAll();
-            })
-            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
-        ;
-        return http.build();
-    }
-}
+//package com.trxyzng.trung.authentication.shared;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//@Order(30)
+//public class DefaultFilterChainConfig {
+//    @Bean
+//    public SecurityFilterChain dddFilterChain(HttpSecurity http) throws Exception {
+//        http
+////            .securityMatcher("")
+//            .cors(Customizer.withDefaults())
+//            .csrf(AbstractHttpConfigurer::disable)
+//            .httpBasic(AbstractHttpConfigurer::disable)
+//            .anonymous(AbstractHttpConfigurer::disable)
+//            .formLogin(AbstractHttpConfigurer::disable)
+//            .logout(AbstractHttpConfigurer::disable)
+//            .authorizeHttpRequests(auth -> {
+//                auth.anyRequest().permitAll();
+//            })
+//            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+//        ;
+//        return http.build();
+//    }
+//}

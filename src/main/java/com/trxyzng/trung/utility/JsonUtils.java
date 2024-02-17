24354040feb3483bafe6c2921de7ce82ea3994a2
trxyzng <trxyzng37@gmail.com>
2024-02-17 13:46:10 +0700

    refactor JsonUtils.java

--- src/main/java/com/trxyzng/trung/utility/JsonUtils.java
+++ src/main/java/com/trxyzng/trung/utility/JsonUtils.java
@@ -4,23 +4,57 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
+import java.util.function.Supplier;
 
 public class JsonUtils {
-    public static ObjectMapper  getObjectMapper() {
+    public static ObjectMapper getObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
     }
-    public static JsonNode getJsonNodeFromString(String input) throws JsonProcessingException {
+
+    public static JsonNode getJsonNodeFromString(String input) {
         ObjectMapper objectMapper = JsonUtils.getObjectMapper();
-        return objectMapper.readTree(input);
+        try {
+            return objectMapper.readTree(input);
+        } catch (JsonProcessingException e) {
+            return JsonNodeFactory.instance.objectNode();
+        }
     }
+
+    public static boolean isEmptyJsonNode(JsonNode jsonNode) {
+        return jsonNode.size() == 0;
+    }
+
     public static String readJsonProperty(JsonNode node, String input) {
         return node.get(input).asText();
     }
-    public static <T> T getJsonObjectFromString(String input, Class<T> classType) throws JsonProcessingException {
+
+    public static <T> T getObjectFromString(String input, Class<T> classType, Supplier<T> supplier) {
         ObjectMapper objectMapper = JsonUtils.getObjectMapper();
-        return objectMapper.readValue(input, classType);
+        try {
+            return objectMapper.readValue(input, classType);
+        } catch (JsonProcessingException e) {
+            return supplier.get();
+        }
+    }
+
+    /**
+     *
+     * @param object
+     * @return String represent the object or ""
+     */
+    public static String getStringFromObject(Object object) {
+        try {
+            ObjectMapper objectMapper = JsonUtils.getObjectMapper();
+            String string = objectMapper.writeValueAsString(object);
+            System.out.println(string);
+            return string;
+        } catch (JsonProcessingException e) {
+            return "";
+        }
     }
 }

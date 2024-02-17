242e637c7b49ae8851e488d2bce257bc739cb111
trxyzng <trxyzng37@gmail.com>
2024-02-17 18:04:40 +0700

    add Login POJO

---
+++ src/main/java/com/trxyzng/trung/authentication/signin/pojo/Login.java
@@ -0,0 +1,12 @@
+package com.trxyzng.trung.authentication.signin.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class Login {
+    private boolean login;
+}

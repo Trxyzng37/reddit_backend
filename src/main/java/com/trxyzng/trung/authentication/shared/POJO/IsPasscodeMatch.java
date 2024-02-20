7c70b415edb6533addb908ddba06fbfa2414d30e
trxyzng <trxyzng37@gmail.com>
2024-02-21 00:02:21 +0700

    add IsPasscodeMatch pojo

---
+++ src/main/java/com/trxyzng/trung/authentication/shared/POJO/IsPasscodeMatch.java
@@ -0,0 +1,16 @@
+package com.trxyzng.trung.authentication.shared.POJO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class IsPasscodeMatch {
+    @JsonProperty("isPasscodeMatch")
+    private boolean isPasscodeMatch;
+    @JsonProperty("isPasscodeExpired")
+    private boolean isPasscodeExpired;
+}

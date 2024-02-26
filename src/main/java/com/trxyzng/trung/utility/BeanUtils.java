063e4e90428576e0fd3b4454bdef5ea1710fe708
trxyzng <trxyzng37@gmail.com>
2024-02-26 20:51:07 +0700

    change BeanUtils notation to @Component

--- src/main/java/com/trxyzng/trung/utility/BeanUtils.java
+++ src/main/java/com/trxyzng/trung/utility/BeanUtils.java
@@ -3,11 +3,11 @@ package com.trxyzng.trung.utility;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 // use to get a bean for inject a spring bean to a plain java class
-
-@Service
+@Component
 public class BeanUtils implements ApplicationContextAware {
     private static ApplicationContext context;
     @Override

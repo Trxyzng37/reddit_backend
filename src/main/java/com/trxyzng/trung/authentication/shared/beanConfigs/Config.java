a623e00df18d0c91139881637f08f4e81cbda98e
trxyzng <trxyzng37@gmail.com>
2024-03-03 20:11:09 +0700

    add cors globally

---
+++ src/main/java/com/trxyzng/trung/authentication/shared/beanConfigs/Config.java
@@ -0,0 +1,22 @@
+package com.trxyzng.trung.authentication.shared.beanConfigs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class Config {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
--- src/main/java/com/trxyzng/trung/authentication/signin/usernamepassword/UsernamePasswordSignInController.java
+++ src/main/java/com/trxyzng/trung/authentication/signin/usernamepassword/UsernamePasswordSignInController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = Constant.frontEndAddress, allowCredentials = "true")
+//@CrossOrigin(origins = Constant.frontEndAddress, allowCredentials = "true")
 //@CrossOrigin
 @RestController
 public class UsernamePasswordSignInController {

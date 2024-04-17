eb1848933e1b8dcd1376669bb7019dc451cf6c4c
trxyzng <trxyzng37@gmail.com>
2024-04-17 15:50:19 +0700

    change get-post controller to get all posts in db

--- src/main/java/com/trxyzng/trung/post/PostService.java
+++ src/main/java/com/trxyzng/trung/post/PostService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PostService {
@@ -22,15 +24,22 @@ public class PostService {
         postRepo.updatePostEntityByPost_id(postId, newContent);
     }
 
-    public GetPostResponse getPostResponseFromId(int post_id) {
-        String type = postRepo.selectTypeFromPostId(post_id);
-        String community_name = postRepo.selectCommunityNameFromPostId(post_id);
-        String community_icon = communityRepo.selectIconFromName(community_name);
-        String username = postRepo.selectUserNameFromPostId(post_id);
-        String title = postRepo.selectTitleFromPostId(post_id);
-        String content = postRepo.selectContentFromPostId(post_id);
-        int vote = postRepo.selectVoteFromPostId(post_id);
-        Instant create_at = postRepo.selectCreatedAtFromPostId(post_id);
-        return new GetPostResponse(post_id, type, username, community_name, "username_icon", community_icon, title, content, create_at, vote);
+    public List<GetPostResponse> getPostResponseFromId(int ppost_id) {
+        int[] post_id_arr = postRepo.selectPostIdFromPostId();
+        List<GetPostResponse> results = new ArrayList<GetPostResponse>();
+        for(int i=0; i<post_id_arr.length; i++) {
+            int post_id = post_id_arr[i];
+            String type = postRepo.selectTypeFromPostId(post_id);
+            String community_name = postRepo.selectCommunityNameFromPostId(post_id);
+            String community_icon = communityRepo.selectIconFromName(community_name);
+            String username = postRepo.selectUserNameFromPostId(post_id);
+            String title = postRepo.selectTitleFromPostId(post_id);
+            String content = postRepo.selectContentFromPostId(post_id);
+            int vote = postRepo.selectVoteFromPostId(post_id);
+            Instant create_at = postRepo.selectCreatedAtFromPostId(post_id);
+            GetPostResponse p = new GetPostResponse(post_id, type, username, community_name, "username_icon", community_icon, title, content, create_at, vote);
+            results.add(p);
+        }
+    return results;
     }
 }

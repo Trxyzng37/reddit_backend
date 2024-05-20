1ff6d7c9783e24d865adf02d49197ec51cca746f
trxyzng <trxyzng37@gmail.com>
2024-05-20 20:22:11 +0700

    add  function to search post title by keyword

--- src/main/java/com/trxyzng/trung/post/get_post/GetPostFilterChain.java
+++ src/main/java/com/trxyzng/trung/post/get_post/GetPostFilterChain.java
@@ -17,7 +17,7 @@ public class GetPostFilterChain {
     @Bean
     public SecurityFilterChain ggetPostFilterChain(HttpSecurity http) throws Exception {
         http
-                .securityMatcher("/get-post", "/get-popular-posts", "get-community-posts", "get-home-posts", "get-control-posts")
+                .securityMatcher("/get-post", "/get-popular-posts", "get-community-posts", "get-home-posts", "get-control-posts", "get-search-posts")
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)

2b7806129641df4906f7183aef253195f1e93945
trxyzng <trxyzng37@gmail.com>
2024-05-27 23:03:20 +0700

    change change password to use filterchain

--- src/main/java/com/trxyzng/trung/authentication/changepassword/change_password/ChangePasswordController.java
+++ src/main/java/com/trxyzng/trung/authentication/changepassword/change_password/ChangePasswordController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://127.0.0.1:4200")
 @RestController
 public class ChangePasswordController {
     @Autowired

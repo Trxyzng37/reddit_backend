3979447ffa985b6e7cd1927a94907280d1600f24
trxyzng <trxyzng37@gmail.com>
2024-06-24 21:31:06 +0700

    add function to remove refresh_token from request

---
+++ src/main/java/com/trxyzng/trung/authentication/signout/SignOutController.java
@@ -0,0 +1,43 @@
+package com.trxyzng.trung.authentication.signout;
+
+import com.trxyzng.trung.authentication.refreshtoken.RefreshTokenService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SignOutController {
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+    @Value("${frontendAddress}")
+    private String frontEndAddress;
+
+    @RequestMapping(value="/sign-out", method = RequestMethod.GET)
+    public ResponseEntity<String> signOut() {
+        try {
+//            Cookie[] cookies = request.getCookies();
+            String refresh_token = "";
+//            for(Cookie cookie: cookies) {
+//                if(cookie.getName().equals("refresh_token")) {
+//                    refresh_token = cookie.getValue();
+//                }
+//            }
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.SET_COOKIE, "refresh_token=" + refresh_token + "; Max-Age=0; SameSite=None; Secure; Path=/; HttpOnly; " +"Domain=" + frontEndAddress);
+            return new ResponseEntity<>("", headers, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.SET_COOKIE, "refresh_token=" + "" + "; Max-Age=0; SameSite=None; Secure; Path=/; HttpOnly; " +"Domain=" + frontEndAddress);
+            return new ResponseEntity<>("", headers, HttpStatus.BAD_REQUEST);
+        }
+    }
+}

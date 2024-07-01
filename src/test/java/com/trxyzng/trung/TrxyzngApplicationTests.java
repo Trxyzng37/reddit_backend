85314ef1b241c8e0a2abaed33de8c19c0cccd589
trxyzng <trxyzng37@gmail.com>
2024-07-01 21:12:46 +0700

    remove tests

--- src/test/java/com/trxyzng/trung/TrxyzngApplicationTests.java
+++ src/test/java/com/trxyzng/trung/TrxyzngApplicationTests.java
@@ -1,13 +1,13 @@
-package com.trxyzng.trung;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TrxyzngApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.trxyzng.trung;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TrxyzngApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

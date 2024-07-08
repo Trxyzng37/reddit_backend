68aff4220626d1d9fe9b18b34cbbef6ea97ec4ae
trxyzng <trxyzng37@gmail.com>
2024-07-08 20:47:39 +0700

    remove SqlConfig.java

--- src/main/java/com/trxyzng/trung/authentication/shared/beanConfigs/SqlConfig.java
+++ src/main/java/com/trxyzng/trung/authentication/shared/beanConfigs/SqlConfig.java
@@ -1,32 +1,32 @@
-package com.trxyzng.trung.authentication.shared.beanConfigs;
-
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class SqlConfig {
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource.sql")
-    public DataSourceProperties sqlDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean
-    @Primary
-    public DataSource sqlDataSource() {
-        return sqlDataSourceProperties()
-                .initializeDataSourceBuilder()
-                .build();
-    }
-
+//package com.trxyzng.trung.authentication.shared.beanConfigs;
+//
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class SqlConfig {
 //    @Bean
-//    public JdbcTemplate sqlTemplate(@Qualifier("sqlDataSource") DataSource dataSource) {
-//        return new JdbcTemplate(dataSource);
+//    @Primary
+//    @ConfigurationProperties("spring.datasource.sql")
+//    public DataSourceProperties sqlDataSourceProperties() {
+//        return new DataSourceProperties();
 //    }
-}
+//
+//    @Bean
+//    @Primary
+//    public DataSource sqlDataSource() {
+//        return sqlDataSourceProperties()
+//                .initializeDataSourceBuilder()
+//                .build();
+//    }
+//
+////    @Bean
+////    public JdbcTemplate sqlTemplate(@Qualifier("sqlDataSource") DataSource dataSource) {
+////        return new JdbcTemplate(dataSource);
+////    }
+//}

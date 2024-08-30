daf8d6fb04c212a33f78c864c51b45ee0f765984
trxyzng <trxyzng37@gmail.com>
2024-08-30 21:13:02 +0700

    change check-refresh-token to return uid if check successful

--- src/main/java/com/trxyzng/trung/authentication/refreshtoken/RefreshTokenController.java
+++ src/main/java/com/trxyzng/trung/authentication/refreshtoken/RefreshTokenController.java
@@ -1,27 +1,19 @@
 package com.trxyzng.trung.authentication.refreshtoken;
 
-import com.trxyzng.trung.authentication.shared.user.UserEntityRepo;
-import com.trxyzng.trung.authentication.shared.user.services.UserEntityService;
-import com.trxyzng.trung.authentication.signin.pojo.GoogleSignInResponse;
-import com.trxyzng.trung.utility.JsonUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-
 @RestController
 public class RefreshTokenController {
+    @Autowired
+    RefreshTokenRepo refreshTokenRepo;
     @Autowired
     private RefreshTokenService refreshTokenService;
     @Value("${frontendAddress}")
@@ -30,23 +22,21 @@ public class RefreshTokenController {
     private String fullFrontendAddress;
 
     @RequestMapping(value="/check-refresh-token", method = RequestMethod.GET)
-    public ResponseEntity<String> checkRefreshToken() {
-//        try {
-//            String refreshToken = RefreshTokenUtil.getRefreshTokenFromCookie(request);
-//            System.out.println("Refresh-token: "+refreshToken);
-//            HttpHeaders headers = new HttpHeaders();
-//            if(refreshToken.length() == 0) {
-//                return new ResponseEntity<>(headers, HttpStatus.UNAUTHORIZED);
-//            }
-//            else {
-//                return new ResponseEntity<>(headers, HttpStatus.OK);
-//            }
-//        }
-//        catch (Exception e) {
-//            HttpHeaders headers = new HttpHeaders();
-//            return new ResponseEntity<>(headers, HttpStatus.BAD_REQUEST);
-//        }
-        HttpHeaders headers = new HttpHeaders();
-        return new ResponseEntity<>(headers, HttpStatus.OK);
+    public ResponseEntity<String> checkRefreshToken(HttpServletRequest request) {
+        try {
+            String refreshToken = RefreshTokenUtil.getRefreshTokenFromCookie(request);
+            HttpHeaders headers = new HttpHeaders();
+            if(refreshToken.length() != 0) {
+                int uid = refreshTokenRepo.selectUidByRefreshToken(refreshToken);
+                return new ResponseEntity<>(String.valueOf(uid), headers, HttpStatus.OK);
+            }
+            else {
+                return new ResponseEntity<>("0", headers, HttpStatus.BAD_REQUEST);
+            }
+        }
+        catch (Exception e) {
+            HttpHeaders headers = new HttpHeaders();
+            return new ResponseEntity<>("0", headers, HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/com/trxyzng/trung/authentication/refreshtoken/RefreshTokenRepo.java
+++ src/main/java/com/trxyzng/trung/authentication/refreshtoken/RefreshTokenRepo.java
@@ -12,6 +12,9 @@ public interface RefreshTokenRepo extends JpaRepository<RefreshTokenEntity, Inte
     @Query("select case when count(t) > 0 then 1 else 0 end from RefreshTokenEntity t where t.refresh_token = :refresh_token")
     int isRefreshTokenExistInDb(@Param("refresh_token") String refresh_token);
 
+    @Query("select t.uid from RefreshTokenEntity t where t.refresh_token = :token")
+    int selectUidByRefreshToken(String token);
+
 //    @Query(nativeQuery = true, value = "SELECT SECURITY.is_id_in_tables(:id)")
 //    boolean isIdExistInTables(@Param("id") int id);
 }
